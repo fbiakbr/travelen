2e17a15ab88730e4babc8545e176ef48e9179b03
fbiakbr <febiakbar19@gmail.com>
2022-05-29 19:34:14 +0700

    Adding service section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-function swapElementsMobile(first, sec) {
-    var first = document.getElementById(first);
-    var sec = document.getElementById(sec);
+function swapElementsMobile(comp1, comp2) {
+    var comp1 = document.getElementById("comp-1");
+    var comp2 = document.getElementById("comp-2");
     if (window.innerWidth < 480) {
-        first.parentNode.insertBefore(sec, first);
+        comp1.parentNode.insertBefore(comp2, comp1);
     }
 }
 
-swapElementsMobile('first', 'sec');
+swapElementsMobile('comp1', 'comp2');
